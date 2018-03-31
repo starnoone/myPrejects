a891ac1eb3bb3ea1ffc1ab7f2a5be233281a4900
starnoone <1428001187@qq.com>
2018-03-31 18:22:58 +0800

    elm

--- elm/src/js/index.js
+++ elm/src/js/index.js
@@ -12,6 +12,8 @@ import api from '../api/indexApi.js';
 const topCateScroll = new BScroll('.top-cate-wrapper',{
 	scrollX:true
 });
+//点击事件失效
+//const firstCateScroll = new BScroll('.wrapper-first');
 
 const catagory = {
 	data:[],
@@ -19,8 +21,8 @@ const catagory = {
 	secondCateIndex : 0
 }
 
-//ajax请求
 $(function(){
+//ajax请求
 	$.ajax({
 		url:api.categoryApi,
 		type:'get',
@@ -35,7 +37,6 @@ $(function(){
 			getCategories(catagory.data);
 		}
 	})
-})
 
 
 //自动添加头部列表信息
@@ -43,12 +44,21 @@ function getTopCateHtml(cate){
 	var cateHtml = "";
 	var cateArr = cate[catagory.firstCateIndex].sub_categories;
 	cateArr.map(function(val, key) {
-		cateHtml += "<a href='javascript:void(0)'>"+val.name+"</a>";
+		var curClass = "";
+		if(key == catagory.secondCateIndex){
+			curClass = "active";
+		}
+		cateHtml += "<a href='javascript:void(0)' class='"+ curClass +"'>"+val.name+"</a>";
 	})
 	$(".top-cate-wrapper .content").html(cateHtml);
 
 	//动态计算content的宽度
-	
+	var cWidth = 0;
+	var a = $('.top-cate-wrapper .content a')
+	a.each(function(i){
+		cWidth += parseInt($(this).innerWidth());
+	});
+	$(".top-cate-wrapper .content").width(cWidth+"px");
 }
 
 //动态添加菜单列表信息
@@ -57,22 +67,68 @@ function getCategories(categories){
 	var firHtml = "";
 	for(let i=1;i<categories.length;i++){
 		var curClass = "";
-		if(i === catagory.firstCateIndex){
+		if(i == catagory.firstCateIndex){
 			curClass = "active";
 		}
-		firHtml += "<li class='"+ curClass +"'>"+categories[i].name+"</li>";
+		firHtml += "<li class='"+ curClass +"' index='"+i+"'>"+categories[i].name+"<span>"+categories[i].count+"</span></li>";
 	}
 	$(".content-first .content").html(firHtml);
 
 	//二级菜单
 	var secHtml = "";
 	var tempCateArr = categories[catagory.firstCateIndex].sub_categories;
+	var suffixArr = ['png','jpg','jpeg','gif'];
 	tempCateArr.map(function(val,k){
 		var curClass = "";
-		if(k === catagory.secondCateIndex){
+		if(k == catagory.secondCateIndex){
 			curClass = "active";
 		}
-		secHtml += "<li class='"+ curClass +"'>"+val.name+"</li>";
+		var img = val.image_url;
+		var suffix = "";
+		for(let i=0;i<suffixArr.length;i++){
+			var startI = img.indexOf(suffixArr[i]);
+			if(startI != -1){
+				suffix = '.'+img.substr(startI);
+			}
+		}
+		var src = 'http://fuss10.elemecdn.com/'+img[0]+'/'+img[1]+img[2]+img.substr(3)+suffix+"?imageMogr/format/webp/thumbnail/!80x80r/gravity/Center/crop/80x80/";
+		//console.log(suffix);
+		secHtml += "<li class='"+ curClass +"' index='"+k+"'><img src='"+src+"'/>"+val.name+"<span>"+val.count+"</span></li>";
 	});
 	$(".content-second .content").html(secHtml);
 }
+
+//菜单切换
+//一级菜单切换
+var firstCateUl = $('.content-first .wrapper-first .content');
+firstCateUl.delegate('li', 'click', function(event) {
+	catagory.firstCateIndex = $(this).attr('index');
+	catagory.secondCateIndex = 0;
+	getCategories(catagory.data);
+});
+//二级菜单
+var secondCateUl = $('.content-second .wrapper .content');
+secondCateUl.delegate('li', 'click', function(event) {
+	_switch('close');
+	catagory.secondCateIndex = $(this).attr('index');
+	//自动添加头部列表信息
+	getTopCateHtml(catagory.data);
+	//动态添加菜单列表信息
+	getCategories(catagory.data);
+});
+
+
+//开
+$('.top-cate-op').click(function(){
+	_switch('open');
+})
+//关
+$('.categories-close').click(function(){
+	_switch('close');
+})
+//封装开关函数
+function _switch(state){
+	$('.top-cate-all').attr('class','top-cate-all '+state);
+}
+
+})
